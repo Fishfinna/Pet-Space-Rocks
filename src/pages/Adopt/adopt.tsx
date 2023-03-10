04bad479a2bf7f583498378f492b54c34b224d74
Shanti <fishfinna12@gmail.com>
2023-03-09 22:01:23 -0800

    update to form

--- src/pages/Adopt/adopt.tsx
+++ src/pages/Adopt/adopt.tsx
@@ -1,22 +1,55 @@
-import React from "react";
 import "./adopt.scss";
 
-
-export default class Adopt extends React.Component {
-
-    render(){
-        return (
-            <div id="adopt">
-            <h1>ADOPT</h1>
-
-            <form>
-                <h2>FiLTER FOR A FRiEND</h2>
-                <label htmlFor="distance">Distance:</label>
-                <input name="distance" type="range"></input>
-                <div className="matching-list"></div>
-                <button type="submit">ADOPT</button>
-            </form>
-            </div>
-        );
-    }
+export default function Adopt() {
+  return (
+    <div id="adopt">
+      <h1 className="text-7xl pt-10 text-center">ADOPT</h1>
+      <h3 className="text-xl pd-10 text-center">FiLTER FOR A FRiEND!</h3>
+      <form>
+        <label htmlFor="distance" className="label text-base-content">
+          Distance From Earth:
+        </label>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          className="range range-xs range-primary w-[500px]"
+        />
+        <div className="form-control ">
+          <label className="label cursor-pointer">
+            <span className="label-text">Hazardous:</span>
+            <input
+              type="radio"
+              name="radio-10"
+              className="radio radio-xs checked:bg-error ml-5"
+              checked
+            />
+          </label>
+          <div className="divider text-xs">OR</div>
+          <label className="label cursor-pointer">
+            <span className="label-text">Safe:</span>
+            <input
+              type="radio"
+              name="radio-10"
+              className="radio radio-xs checked:bg-info ml-5"
+              checked
+            />
+          </label>
+        </div>
+        <select className="select select-sm select-primary w-full max-w-xs">
+          <option disabled selected>
+            Size
+          </option>
+          <option>Small</option>
+          <option>Medium</option>
+          <option>Large</option>
+          <option>Any</option>
+        </select>
+        <div className="matching-list"></div>
+        <button type="submit" className="btn btn-primary">
+          ADOPT
+        </button>
+      </form>
+    </div>
+  );
 }
