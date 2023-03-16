39a1434d32dda6e6c24f8c80d904d4df12e90624
Shanti <fishfinna12@gmail.com>
2023-03-15 19:48:06 -0700

    wizard start

--- src/pages/Adopt/adopt.tsx
+++ src/pages/Adopt/adopt.tsx
@@ -1,27 +1,13 @@
 import "./adopt.scss";
-import * as React from "react";
-import { Wizard, useWizard } from "react-use-wizard";
+import { Wizard } from "react-use-wizard";
+
+// wizard steps
+import Name from "./wizard/name";
 
 export default function About(): JSX.Element {
   return (
-    <Wizard>
-      <Step1 />
+    <Wizard wrapper={<form id="wrapper" />}>
+      <Name />
     </Wizard>
   );
 }
-
-const Step1 = () => {
-  const { handleStep, previousStep, nextStep } = useWizard();
-
-  // Attach an optional handler
-  handleStep(() => {
-    alert("Going to step 2");
-  });
-
-  return (
-    <>
-      <button onClick={() => previousStep()}>Previous ⏮️</button>
-      <button onClick={() => nextStep()}>Next ⏭</button>
-    </>
-  );
-};
---
+++ src/pages/Adopt/wizard/name.tsx
@@ -0,0 +1,19 @@
+import { useWizard } from "react-use-wizard";
+
+export default function Name() {
+  let { nextStep } = useWizard();
+
+  return (
+    <>
+      <h1 className="text-5xl mb-12">Name your new pet!</h1>
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input input-bordered input-accent w-full max-w-xs"
+      />
+      <button onClick={() => nextStep()} className="w-full max-w-xs rounded-xl">
+        Next
+      </button>
+    </>
+  );
+}
