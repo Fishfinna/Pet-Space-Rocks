be4ed99f5c365a64aa818f311922e7f45b825d76
Shanti <fishfinna12@gmail.com>
2023-03-19 13:45:57 -0700

    updated the form

--- src/pages/Adopt/wizard/danger.tsx
+++ src/pages/Adopt/wizard/danger.tsx
@@ -1,25 +1,37 @@
 import { useWizard } from "react-use-wizard";
 
 export default function Danger() {
-  let { nextStep } = useWizard();
+  let { nextStep, previousStep } = useWizard();
 
   return (
     <>
       <h1 className="text-5xl mb-12">Would you like a Danger Rock?</h1>
-      <div>
-        <label>Safe:</label>
-        <input type="radio" name="radio" className="radio radio-info" />
-      </div>
-      <div>
-        <label>Hazardous:</label>
-        <input type="radio" name="radio" className="radio radio-error" />
+      <div className="flex flex-col">
+        <div className="labeled">
+          <label>Either:</label>
+          <input type="radio" name="radio" className="radio" />
+        </div>
+        <div className="labeled">
+          <label>Safe:</label>
+          <input type="radio" name="radio" className="radio radio-info" />
+        </div>
+        <div className="labeled">
+          <label>Hazardous:</label>
+          <input type="radio" name="radio" className="radio radio-error" />
+        </div>
       </div>
       <button
         onClick={() => nextStep()}
-        className="w-full max-w-xs rounded-xl "
+        className="w-full max-w-xs rounded-xl m-10"
       >
         Next
       </button>
+      <a
+        onClick={() => previousStep()}
+        className=" text-gray-400 underline hover:no-underline cursor-pointer"
+      >
+        Back
+      </a>
     </>
   );
 }
