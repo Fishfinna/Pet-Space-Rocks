cff70391811dbcadc316daa74e809e89a2bcbe91
Shanti <fishfinna12@gmail.com>
2023-03-29 18:26:36 -0700

    yep

--- src/pages/Adopt/wizard/select.tsx
+++ src/pages/Adopt/wizard/select.tsx
@@ -6,15 +6,7 @@ export default function Name() {
   return (
     <>
       <h1 className="text-5xl mb-12">Select your Pet!</h1>
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-accent w-full max-w-xs focus:input-primary"
-      />
-      <button
-        onClick={() => nextStep()}
-        className="w-full max-w-xs rounded-xl "
-      >
+      <button onClick={() => nextStep()} className="w-full max-w-xs rounded-xl">
         Next
       </button>
     </>
