7065e7b8972d93de6a3a8f5a4c6620a3ff56c2cd
Shanti <fishfinna12@gmail.com>
2023-04-04 19:09:10 -0700

    added

--- src/components/List/Stacks/Item/item.tsx
+++ src/components/List/Stacks/Item/item.tsx
@@ -1,6 +1,6 @@
 export default function Item() {
   return (
-    <div className="card shadow-md bg-primary text-primary-content">
+    <div className="card shadow-md bg-accent text-accent-content">
       <div className="card-body">
         <h2 className="card-title">Notification 1</h2>
         <p>You have 3 unread messages. Tap here to see.</p>
