3fffd6b4af5c991149d025a776f0d40e0aafbed1
Shanti <fishfinna12@gmail.com>
2023-04-16 13:40:55 -0700

    wizard animations

--- src/pages/Adopt/adopt.tsx
+++ src/pages/Adopt/adopt.tsx
@@ -1,5 +1,6 @@
 import "./adopt.scss";
 import { Wizard } from "react-use-wizard";
+import { motion } from "framer-motion";
 
 // wizard steps
 import Name from "./wizard/name";
@@ -7,13 +8,24 @@ import Danger from "./wizard/danger";
 import Distance from "./wizard/distance";
 import Select from "./wizard/select";
 
+let pages = [Name, Danger, Distance, Select];
+
 export default function About(): JSX.Element {
   return (
-    <Wizard wrapper={<form id="wrapper" />}>
-      <Name />
-      <Distance />
-      <Danger />
-      <Select />
+    <Wizard>
+      {pages.map((Page) => (
+        <motion.div
+          initial={{ x: 10, y: 10, opacity: "0%" }}
+          animate={{
+            x: 0,
+            y: 0,
+            opacity: "100%",
+          }}
+          className="wrapper overflow-hidden max-h-[99vh] max-w-[99vw]"
+        >
+          <Page />
+        </motion.div>
+      ))}
     </Wizard>
   );
 }
